14b6a81757a6a914ecf9de5ea143d13890da34cf
Muhammad Owais Sherwani <51533702+devmowais@users.noreply.github.com>
2022-12-27 17:32:08 +0500

    Done!!!

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,16 +9,16 @@ export const About = () => {
         <div className='about-left-content'>
           <div>
             <div className='shadow'>
-              <div className='about-img'><img src='https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg=' /></div>
+              <div className='about-img'><img src="https://media.licdn.com/dms/image/D4D03AQHQsrIF3ceRJg/profile-displayphoto-shrink_800_800/0/1672139101339?e=1677715200&v=beta&t=6Eq4kMaHOhh_J8VfP4YAKv_R5ctn1pinOsBD0cJthHs" /></div>
             </div>
             <h2>Owais Sherwani</h2>
             <h3>Web Developer</h3>
           </div>
           <ul className="icons">
-            <li><i className='fab fa-twitter'></i></li>
-            <li><i className='fab fa-linkedin'></i></li>
-            <li><i className='fab fa-instagram'></i></li>
-            <li><i className='fab fa-facebook-f'></i></li>
+            <li><a href='https://twitter.com/' target="_blank" rel="noreferrer"><i className='fab fa-twitter'></i></a></li>
+            <li><a href='https://linkedin.com/' target="_blank" rel="noreferrer"><i className='fab fa-linkedin'></i></a></li>
+            <li><a href='https://instagram.com/' target="_blank" rel="noreferrer"><i className='fab fa-instagram'></i></a></li>
+            <li><a href='https://facebook.com/' target="_blank" rel="noreferrer"><i className='fab fa-facebook-f'></i></a></li>
           </ul>
         </div>
       </div>
@@ -32,8 +32,8 @@ export const About = () => {
         <button type="button" className='btnn btnn-white'>Git hub</button></a>
         </div>
         <div className='about-para'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, aspernatur possimus ullam quaerat, laboriosam ex voluptate aliquid laborum, obcaecati ratione accusamus! Ea nisi modi dolor nam numquam? Temporibus, molestias amet.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus iure tempora alias laudantium sapiente impedit!</p>
+          <p> I'm a self-taught passionate <span className='clrChg'>Front-end Web Developer</span> from <span className='pak'>Pakistan</span> 🇵🇰.</p>
+          <p>I have a diverse set of skills ranging from design, to <span className='clrChg'>HTML + CSS + JavaScript + React</span>... <br/> I also have a good understanding with <span className='clrChg'>Bootsrap and Tailwind CSS</span> framework... <br/> And, last but not least, Learning and Exploring <span className='clrChg'><u>Blockchain</u></span> development with <span className='clrChg'>Solidity</span>...</p>
         </div>
       </div>
     </div>
